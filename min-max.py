0ac76ecef838f0660b156c384ff17a959c1dda53
sssa13 <alexwork13@ukr.net>
2019-05-26 11:21:39 +0300

    edited min - max

--- min-max.py
+++ min-max.py
@@ -2,8 +2,11 @@ def min_max(str1):
     list_a = [int(i) for i in str1.split()]
     min1 = min(list_a)
     max1 = max(list_a)
-    list_a[list_a.index(min1)] = max1
-    list_a[list_a.index(max1)] = min1
+    index1 = list_a.index(max1)
+    index2 = list_a.index(min1)
+    list_a[index1] = min1
+    list_a[index2] = max1
+    
     return " ".join([str(i) for i in list_a])
 
 if __name__ == "__main__":
